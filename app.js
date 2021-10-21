849851950f1d5f404f10a0c26b7fc5d236bb4ed1
Fabio <alessf13@gmail.com>
2021-10-21 13:54:19 +0200

    feat: color nav animation added

--- app.js
+++ app.js
@@ -17,3 +17,20 @@ links.forEach((link) => {
     burger.classList.toggle("toggle");
   });
 });
+
+nav.addEventListener(
+  "mouseover",
+  function (e) {
+    console.log(e.target.innerText);
+    if (e.target.innerText === "About") {
+      head.style.boxShadow = "0px 10px 10px rgb(226, 230, 7)";
+    } else if (e.target.innerText === "Work") {
+      head.style.boxShadow = "0px 10px 10px rgb(7, 11, 230)";
+    } else if (e.target.innerText === "Contact") {
+      head.style.boxShadow = "0px 10px 10px rgb(230, 33, 7)";
+    } else {
+      head.style.boxShadow = "0px 10px 10px rgba(235, 231, 231, 0.4)";
+    }
+  },
+  false
+);
